c2e8f6708e445f4e5b7a0068b7117ce178469d9e
minnowfeather <matthewparedes2k3@gmail.com>
2022-10-05 19:27:03 -0700

    Basic tictactoe features implemented

--- tictactoe.c
+++ tictactoe.c
@@ -7,6 +7,38 @@ void printOptions(){
 
 void tictactoe(){
 
+	// print dialog
+	printf("First or second (1 = first, 2 = second): ");
+	int turnCounter;
+	scanf("%d", &turnCounter);
+
+	// initialize board to be empty
+	int board[3][3];
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			board[i][j] = 0;
+		}
+	}
+
+	// track if game is finished
+	bool gameFinished = false;
+	int row;
+	int column
+	// game main loop
+	while(!gameFinished){
+
+		printf("Input the row: ");
+		scanf("%d", &row);
+		printf("Input the column: ");
+		scanf("%d", &column);
+		board[column][row] = turnCounter;
+		printBoard(&board);
+		if(turnCounter == 1){
+			turnCounter == 2;
+		} else {
+			turnCounter = 1;
+		}
+	}
 }
 
 int main(){
